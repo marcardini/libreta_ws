f1c79054717e2b5bb78e5ea4a0b248d9265f091a
marcardini <arcardinimathias@gmail.com>
2017-02-11 23:43:53 -0300

    bajar cambios

--- libretaDigital/scripts/1.0.0.0/script_basic_test_data.sql
+++ libretaDigital/scripts/1.0.0.0/script_basic_test_data.sql
@@ -17,7 +17,7 @@ insert into group_ values (1,'1A',2016,null,0)
 go
 update notebook set group_id = 1 where oid = 1
 go
-insert into professor values (1, 'maria', 'tarigo', '1990-06-20 00:00:00', 'FEMALE', 'maria.tarigo@gmail.com', 'admin', 'GRADE_5', '2016-01-01 00:00:00', null, 1, 0)
+insert into professor values (1, 'teresita', 'gomez', '1990-06-20 00:00:00', 'FEMALE', 'teresita.gomez@gmail.com', 'admin', 'GRADE_5', '2016-01-01 00:00:00', null, 1, 0, '094555996')
 go
 insert into course values (1,'primero',1,0)
 go
@@ -27,45 +27,45 @@ update subject set group_id = 1 where oid = 1
 go
 update notebook set professor_id = 1 where oid = 1
 go
-insert into student values (1,'matias', 'hernandez', '1989-01-01 00:00:00', 'MALE', 'mati@gmail.com', true, 1, 0, 1)
+insert into student values (1,'matias', 'hernandez', '1989-01-01 00:00:00', 'MALE', 'mati@gmail.com', true, 1, 0, 1, '098563741')
 go
-insert into student values (2,'mathias', 'arcardini', '1989-05-17 00:00:00', 'MALE', 'arcardinimathias@gmail.com', true, 1, 1, 1)
+insert into student values (2,'mathias', 'arcardini', '1989-05-17 00:00:00', 'MALE', 'arcardinimathias@gmail.com', true, 1, 1, 1, '091377334')
 go
-insert into student values (3,'laura', 'pereira', '1993-02-08 00:00:00', 'FEMALE', 'laura@gmail.com', true, 1, 2, 1)
+insert into student values (3,'laura', 'pereira', '1993-02-08 00:00:00', 'FEMALE', 'laura@gmail.com', true, 1, 2, 1, '099653455')
 go
-insert into student values (4,'jorge', 'berro', '1974-11-30 00:00:00', 'MALE', 'berro@gmail.com', true, 1, 3, 1)
+insert into student values (4,'jorge', 'berro', '1974-11-30 00:00:00', 'MALE', 'berro@gmail.com', true, 1, 3, 1, '091586483')
 go
-insert into student values (5,'vanesa', 'perez', '1954-03-20 00:00:00', 'FEMALE', 'vaneperez@gmail.com', true, 1, 4, 1)
+insert into student values (5,'vanesa', 'perez', '1954-03-20 00:00:00', 'FEMALE', 'vaneperez@gmail.com', true, 1, 4, 1, '097563783')
 go
-insert into class_day_student values(1,'2016-11-24 00:00:00', 'FALTA', 1,1,1,1,null,null)
+insert into class_day_student values(1,'2017-02-06 00:00:00', 'FALTA', 1,1,1,1,null,null)
 go
-insert into class_day_student values(2,'2016-11-25 00:00:00', 'MEDIA_FALTA', 1,1,1,1,null,null)
+insert into class_day_student values(2,'2017-02-07 00:00:00', 'MEDIA_FALTA', 1,1,1,1,null,null)
 go
-insert into class_day_student values(3,'2016-11-24 00:00:00', 'FALTA', 1,1,1,1,null,null)
+insert into class_day_student values(3,'2017-02-06 00:00:00', 'FALTA', 1,1,1,1,null,null)
 go
-insert into class_day_student values(4,'2016-11-25 00:00:00', 'MEDIA_FALTA', 1,1,1,1,null,null);
+insert into class_day_student values(4,'2017-02-07 00:00:00', 'MEDIA_FALTA', 1,1,1,1,null,null);
 go
-insert into class_day_student values(5,'2016-11-24 00:00:00', 'FALTA', 1,1,1,1,null,null);
+insert into class_day_student values(5,'2017-02-06 00:00:00', 'FALTA', 1,1,1,1,null,null);
 go
-insert into class_day_student values(6,'2016-11-25 00:00:00', 'MEDIA_FALTA', 1,1,1,1,null,null);
+insert into class_day_student values(6,'2017-02-07 00:00:00', 'MEDIA_FALTA', 1,1,1,1,null,null);
 go
-insert into class_day_student values(7,'2016-11-24 00:00:00', 'FALTA', 1,1,1,1,null,null);
+insert into class_day_student values(7,'2017-02-06 00:00:00', 'FALTA', 1,1,1,1,null,null);
 go
-insert into class_day_student values(8,'2016-11-25 00:00:00', 'MEDIA_FALTA', 1,1,1,1,null,null);
+insert into class_day_student values(8,'2017-02-07 00:00:00', 'MEDIA_FALTA', 1,1,1,1,null,null);
 go
-insert into class_day_student values(9,'2016-11-24 00:00:00', 'FALTA', 1,1,1,1,null,null);
+insert into class_day_student values(9,'2017-02-06 00:00:00', 'FALTA', 1,1,1,1,null,null);
 go
-insert into class_day_student values(10,'2016-11-25 00:00:00', 'MEDIA_FALTA', 1,1,1,1,null,null);
+insert into class_day_student values(10,'2017-02-07 00:00:00', 'MEDIA_FALTA', 1,1,1,1,null,null);
 go
-insert into class_day_student values(11,'2016-11-25 00:00:00', 'EXAMEN', 1,1,1,1,6,'pobre')
+insert into class_day_student values(11,'2017-02-06 00:00:00', 'EXAMEN', 1,1,1,1,6,'pobre')
 go
-insert into class_day_student values(12,'2016-11-25 00:00:00', 'EXAMEN', 1,1,1,1,7,'pobre')
+insert into class_day_student values(12,'2017-02-07 00:00:00', 'EXAMEN', 1,1,1,1,7,'pobre')
 go
-insert into class_day_student values(13,'2016-11-25 00:00:00', 'EXAMEN', 1,1,1,1,8,'pobre')
+insert into class_day_student values(13,'2017-02-06 00:00:00', 'EXAMEN', 1,1,1,1,8,'pobre')
 go
-insert into class_day_student values(14,'2016-11-25 00:00:00', 'EXAMEN', 1,1,1,1,9,'bien')
+insert into class_day_student values(14,'2017-02-07 00:00:00', 'EXAMEN', 1,1,1,1,9,'bien')
 go
-insert into class_day_student values(15,'2016-11-25 00:00:00', 'EXAMEN', 1,1,1,1,10,'excelente')
+insert into class_day_student values(15,'2017-02-06 00:00:00', 'EXAMEN', 1,1,1,1,10,'excelente')
 go
 insert into roles values(1, 'ADMIN')
 go
